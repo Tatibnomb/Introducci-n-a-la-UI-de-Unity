2db317dada60e9c3f02d1ec9f4f1b6d902c0a01f
Tatibnomb <49379010@est.ort.edu.ar>
2025-04-24 10:17:05 -0300

    02 ¿terminado?

---
+++ Assets/Scripts/MayoriaDeEdad.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MayoriaDeEdad : MonoBehaviour {
+
+    int edadUsuario;
+
+    void Start() {
+
+        edadUsuario = 16;
+
+        if (edadUsuario >= 18)
+        {
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            Debug.Log("Es menor de edad");
+        }
+    }
+
+}
